15234e219c32d00c45efcfc4393582bfbf638a53
Sam Kirsch <samdkirsch@gmail.com>
2017-03-03 08:30:16 -0800

    create Venue.GetAll() pass VenueDBStartsEmpty test

--- Objects/Venue.cs
+++ Objects/Venue.cs
@@ -16,24 +16,24 @@ namespace BandTrackerApp
             _id = newId;
         }
 
-        // public static List<Venue> GetAll()
-        // {
-        //     List<Venue> allVenues = new List<Venue>{};
-        //
-        //     SqlConnection conn = DB.Connection();
-        //     conn.Open();
-        //
-        //     SqlCommand cmd = new SqlCommand("SELECT * FROM venues;", conn);
-        //     SqlDataReader rdr = cmd.ExecuteReader();
-        //
-        //     while(rdr.Read())
-        //     {
-        //         allVenues.Add(new Venue(rdr.GetString(1), rdr.GetInt32(0)));
-        //     }
-        //
-        //     DB.CloseSqlConnection(conn, rdr);
-        //     return allVenues;
-        // }
+        public static List<Venue> GetAll()
+        {
+            List<Venue> allVenues = new List<Venue>{};
+
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM venues;", conn);
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            while(rdr.Read())
+            {
+                allVenues.Add(new Venue(rdr.GetString(1), rdr.GetInt32(0)));
+            }
+
+            DB.CloseSqlConnection(conn, rdr);
+            return allVenues;
+        }
 
         public static void DeleteAll()
         {
