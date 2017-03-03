0c4a7ac438d692024337a8d0ffc34e64ddac5f78
Sam Kirsch <samdkirsch@gmail.com>
2017-03-03 11:37:31 -0800

    more routes and styling

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -13,8 +13,46 @@ namespace BandTrackerApp
         {
             Get["/"] = _ =>
             {
-                return View["index.cshtml"];
+                return View["index.cshtml", ModelMaker()];
             };
+
+            Post["/bands"] = _ =>
+            {
+                Band newBand = new Band(Request.Form["new-band"]);
+                newBand.Save();
+                return View["index.cshtml", ModelMaker()];
+            };
+
+            Post["/venues"] = _ =>
+            {
+                Venue newVenue = new Venue(Request.Form["new-venue"]);
+                newVenue.Save();
+                return View["index.cshtml", ModelMaker()];
+            };
+
+            Get["/bands/{id}"] = parameters =>
+            {
+                Dictionary<string, object> model = ModelMaker();
+                model.Add("band", Band.Find(parameters.id));
+                return View["band.cshtml", model];
+            };
+
+            Get["/venues/{id}"] = parameters =>
+            {
+                Dictionary<string, object> model = ModelMaker();
+                model.Add("venue", Venue.Find(parameters.id));
+                return View["venue.cshtml", model];
+            };
+        }
+
+        public static Dictionary<string, object> ModelMaker()
+        {
+            Dictionary<string, object> model = new Dictionary<string, object>
+            {
+                {"bands", Band.GetAll()},
+                {"venues", Venue.GetAll()}
+            };
+            return model;
         }
     }
 }
