5c9a060984bbd0f826cb0e9796b384fd68646356
Sam Kirsch <samdkirsch@gmail.com>
2017-03-03 08:27:13 -0800

    pass BandStartsEmpty test create GetAll() method for Band

--- Objects/Band.cs
+++ Objects/Band.cs
@@ -6,11 +6,38 @@ namespace BandTrackerApp
 {
     public class Band
     {
-        // Create Band Here
+        private string _name;
+        private int _id;
+
+        public Band(string newString, int newId = 0)
+        {
+            _name = newString;
+            _id = newId;
+        }
+
+        public static List<Band> GetAll()
+        {
+            List<Band> allBands = new List<Band>{};
+
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM bands;", conn);
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            while(rdr.Read())
+            {
+                allBands.Add(new Band(rdr.GetString(1), rdr.GetInt32(0)));
+            }
+
+            DB.CloseSqlConnection(conn, rdr);
+            return allBands;
+        }
+
 
         public static void DeleteAll()
         {
-            // DB.DeleteAll("bands");
+            DB.DeleteAll("bands");
         }
     }
 }
