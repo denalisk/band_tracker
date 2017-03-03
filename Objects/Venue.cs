4517eba73858cd760c3b42d6fac36c66373316f6
Sam Kirsch <samdkirsch@gmail.com>
2017-03-03 08:29:28 -0800

    create failing test for VenueDBStartsEmpty

--- Objects/Venue.cs
+++ Objects/Venue.cs
@@ -7,10 +7,37 @@ namespace BandTrackerApp
     public class Venue
     {
         // Create Venue Here
+        private string _name;
+        private int _id;
+
+        public Venue(string newName, int newId)
+        {
+            _name = newName;
+            _id = newId;
+        }
+
+        // public static List<Venue> GetAll()
+        // {
+        //     List<Venue> allVenues = new List<Venue>{};
+        //
+        //     SqlConnection conn = DB.Connection();
+        //     conn.Open();
+        //
+        //     SqlCommand cmd = new SqlCommand("SELECT * FROM venues;", conn);
+        //     SqlDataReader rdr = cmd.ExecuteReader();
+        //
+        //     while(rdr.Read())
+        //     {
+        //         allVenues.Add(new Venue(rdr.GetString(1), rdr.GetInt32(0)));
+        //     }
+        //
+        //     DB.CloseSqlConnection(conn, rdr);
+        //     return allVenues;
+        // }
 
         public static void DeleteAll()
         {
-            // DB.DeleteAll("venues");
+            DB.DeleteAll("venues");
         }
     }
 }
